c2e9e4881e699115484ba24a27e0a3a9dcfd00ee
Joe Adams <adams10301@gmail.com>
2015-02-05 22:59:19 -0500

    Added parsing for auditd config file. Also added some STIG tests for auditd configuration.

--- tests/audit.py
+++ tests/audit.py
@@ -1,29 +1,56 @@
-from utils import console
+from utils import console, parsing
+
+
+class auditd():
+
+    def __init__(self):
+        self.auditd_config_file = '/etc/audit/auditd.conf'
+        self.auditd_config = parsing.parse_auditd_config(self.auditd_config_file)
+        return
+
+    def check_admin_space_left_action(self):
+        # RHEL-06-000163
+        if self.auditd_config['admin_space_left_action'].upper() != "SINGLE":
+            console.error("Auditd admin_space_left_action is not set to 'SINGLE'!")
+            return None
+
+    def check_spece_left_action(self):
+        # RHEL-06-000005
+        if self.auditd_config['space_left_action'] == "EMAIL":
+            console.ok("Free space warning enabled in auditd")
+            return None
+        elif self.auditd_config['space_left_action'] == "SYSLOG":
+            console.warning("Free space warning is set to SYSLOG. Make sure this notifies the necessary individuals in a timely manner")
+            return None
+        else:
+            console.error("Free space warning not enabled in auditd")
+            return None
+
+    def check_max_log_file_action(self):
+        # RHEL-06-000161
+        if self.auditd_config['max_log_file_action'].upper() != "ROTATE":
+            console.error("Auditd max_log_file_actoin not set to 'ROTATE'!")
+
+    def check_max_log_file(self):
+        # RHEL-06-000160
+        #  This rule may need to be altered because the STIG only defines this
+        #  must be set large enough to meet the retention period required.
+        if not int(self.auditd_config['max_log_file']) >= 6:
+            console.error("Auditd max_log_file not set to at least 6!")
+
+    def check_num_logs(self):
+        # RHEL-06-000159
+        #  This rule may need to be altered because the STIG only defines this
+        #  must be set large enough to meet the retention period required.
+        if not int(self.auditd_config['num_logs']) >= 5:
+            console.error("Auditd num_logs not set to at least 5!")
 
 
 def run_tests():
 
-    # RHEL-06-000005
-    with open("/etc/audit/auditd.conf") as audit_config_file:
-        audit_config = audit_config_file.readlines()
-        for line in audit_config:
-
-            line.strip()
-            if not line.startswith("#"):
-
-                if line.startswith("space_left_action"):
-                    key, value = line.split(" = ")
-
-                    if value == "EMAIL":
-                        console.ok("Free space warning enabled in auditd")
-                        return None
-
-                    elif value == "SYSLOG":
-                        console.warning("Free space warning is set to SYSLOG. Make sure this notifies the necessary individuals in a timely manner")
-
-                    else:
-                        console.error("Free space warning not enabled in auditd")
-                        return None
-
-        console.error("Free space warning not enabled in auditd")
-        return None
+    audit = auditd()
+    audit.check_admin_space_left_action()
+    audit.check_spece_left_action()
+    audit.check_max_log_file_action()
+    audit.check_max_log_file()
+    audit.check_num_logs()
--- utils/parsing.py
+++ utils/parsing.py
@@ -22,3 +22,26 @@ def parse_rsyslog_config(location):
             file_values[k] = v.strip()
 
     return file_values
+
+
+def parse_auditd_config(location):
+    with open(location, 'r') as infile:
+        file_values = {}
+        lines = infile.readlines()
+
+        for line in lines:
+            # Remove whitespace in line
+            line = line.strip()
+
+            # Remove empty lines and comments
+            if not line or line.startswith("#"):
+                continue
+
+            # Split lines on '='
+            k, v = line.split('=', 1)
+            k = k.strip()
+            v = v.strip()
+
+            file_values[k] = v.strip()
+
+    return file_values
